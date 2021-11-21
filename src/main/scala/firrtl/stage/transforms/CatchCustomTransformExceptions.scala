08b96da4aaf27cb6f41f354aa63778b4f25cfad4
sinofp <sinofp@tuta.io>
2021-11-21 16:38:00 +0000

    Fix test after deleted Driver

--- src/main/scala/firrtl/stage/transforms/CatchCustomTransformExceptions.scala
+++ src/main/scala/firrtl/stage/transforms/CatchCustomTransformExceptions.scala
@@ -9,11 +9,23 @@ class CatchCustomTransformExceptions(val underlying: Transform) extends Transfor
   override def execute(c: CircuitState): CircuitState = try {
     underlying.transform(c)
   } catch {
-    case e: Exception => throw CustomTransformException(e)
+    case e: Exception if CatchCustomTransformExceptions.isCustomTransform(trueUnderlying) =>
+      throw CustomTransformException(e)
   }
 
 }
 
 object CatchCustomTransformExceptions {
+
+  private[firrtl] def isCustomTransform(xform: Transform): Boolean = {
+    def getTopPackage(pack: java.lang.Package): java.lang.Package =
+      Package.getPackage(pack.getName.split('.').head)
+    // We use the top package of the Parser to get the top firrtl package
+    Option(xform.getClass.getPackage).forall { p =>
+      getTopPackage(p) != firrtl.Parser.getClass.getPackage
+    }
+  }
+
   def apply(a: Transform): CatchCustomTransformExceptions = new CatchCustomTransformExceptions(a)
+
 }
